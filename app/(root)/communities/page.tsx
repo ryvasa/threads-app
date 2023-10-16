5331cc97142116e6b0f24ad0cc5d92d3ae699309
ryvasa <ryanoktaviansaputra9@gmail.com>
2023-10-16 21:47:09 +0700

    search and pagination

--- app/(root)/communities/page.tsx
+++ app/(root)/communities/page.tsx
@@ -1,8 +1,8 @@
 import { currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
-// import Searchbar from "@/components/shared/Searchbar";
-// import Pagination from "@/components/shared/Pagination";
+import Searchbar from "@/components/shared/SearchBar";
+import Pagination from "@/components/shared/Pagination";
 import CommunityCard from "@/components/cards/CommunityCard";
 
 import { fetchUser } from "@/lib/actions/user.actions";
@@ -29,7 +29,9 @@ const Page = async ({
     <>
       <h1 className="head-text">Communities</h1>
 
-      <div className="mt-5">{/* <Searchbar routeType='communities' /> */}</div>
+      <div className="mt-5">
+        <Searchbar routeType="communities" />
+      </div>
 
       <section className="mt-9 flex flex-wrap gap-4">
         {result.communities.length === 0 ? (
@@ -51,11 +53,11 @@ const Page = async ({
         )}
       </section>
 
-      {/* <Pagination
-        path='communities'
+      <Pagination
+        path="communities"
         pageNumber={searchParams?.page ? +searchParams.page : 1}
         isNext={result.isNext}
-      /> */}
+      />
     </>
   );
 };
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,10 @@
 import { currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
-// import ThreadCard from "@/components/cards/ThreadCard";
-// import Pagination from "@/components/shared/Pagination";
-
+import Pagination from "@/components/shared/Pagination";
 import { fetchPosts } from "@/lib/actions/thread.actions";
 import ThreadCard from "@/components/cards/ThreadCard";
-// import { fetchUser } from "@/lib/actions/user.actions";
+import { fetchUser } from "@/lib/actions/user.actions";
 
 async function Home({
   searchParams,
@@ -14,10 +12,10 @@ async function Home({
   searchParams: { [key: string]: string | undefined };
 }) {
   const user = await currentUser();
-  // if (!user) return null;
+  if (!user) return null;
 
-  // const userInfo = await fetchUser(user.id);
-  // if (!userInfo?.onboarded) redirect("/onboarding");
+  const userInfo = await fetchUser(user.id);
+  if (!userInfo?.onboarded) redirect("/onboarding");
 
   const result = await fetchPosts(
     searchParams.page ? +searchParams.page : 1,
@@ -50,11 +48,11 @@ async function Home({
         )}
       </section>
 
-      {/* <Pagination
-        path='/'
+      <Pagination
+        path="/"
         pageNumber={searchParams?.page ? +searchParams.page : 1}
         isNext={result.isNext}
-      /> */}
+      />
     </>
   );
 }
